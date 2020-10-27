e6e4a024aa956ca36e591559219e727b923367ad
prathyushas31 <prathyushas1111@gmail.com>
2020-10-27 22:22:09 +0530

    store the results in dictionary for every computation

--- arthematicsorting.sh
+++ arthematicsorting.sh
@@ -1,6 +1,22 @@
-read -p "Enter number 1 :" a
-read -p "Enter number 2 :" b
-read -p "Enter number 3 :" c
+echo "Welcome to arthmetic operations"
+read -p "Enter number 1:" a
+read -p "Enter number 2:" b
+read -p "Enter number 3:" c
+computation1=$(( $a+$b*$c ))
+echo "first computation is:" $computation1
+computation2=$(( $a*$b+$c ))
+echo "second Computation is:"$computation2
+computation3=$(( $c+$a/$b ))
+echo "third computation is:"$computation3
 computation4=$(( $a%$b+$c ))
 echo "Fourth Computation is:"$computation4
+declare -A computations
+computation[1]=$computation1
+computation[2]=$computation2
+computation[3]=$computation3
+computation[4]=$computation4
+
+echo "dictionary values are :" ${computation[@]}
+
+
 
