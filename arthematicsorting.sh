517f6d933ba4386a772861c4c238119b3ce1958b
prathyushas31 <prathyushas1111@gmail.com>
2020-10-27 22:17:09 +0530

    compute a%b+c

--- arthematicsorting.sh
+++ arthematicsorting.sh
@@ -1,5 +1,6 @@
 read -p "Enter number 1 :" a
 read -p "Enter number 2 :" b
 read -p "Enter number 3 :" c
-computation3=$(( $c+$a/$b ))
-echo "third computation is:"$computation3
+computation4=$(( $a%$b+$c ))
+echo "Fourth Computation is:"$computation4
+
